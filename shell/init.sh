c6e4d0bdc301d7070f75562c8c1aa687a39a5f3e
Jairo B² Rauwers <jairorauwers@gmail.com>
2024-01-12 13:16:44 -0300

    new feat

--- shell/init.sh
+++ shell/init.sh
@@ -1,1 +1,11 @@
 
+
+CONFIGS=$HOME/.shellscript
+# source $CONFIGS/init.sh
+
+FILES_STR=$(fd --glob '*.sh' --exclude 'init.sh' $CONFIGS)
+FILES=($(echo $FILES_STR | tr '\n' ' '))   
+
+for FILE in $FILES; do
+    source $FILE
+done
